170d442c0ffd0856747c7c990517a7916caada4a
Oom Sveta <oomsveta@protonmail.com>
2020-12-18 11:58:25 +0100

    Add flag constants for the parser

--- include/ft_printf.h
+++ include/ft_printf.h
@@ -13,7 +13,20 @@
 #ifndef FT_PRINTF_H
 #define FT_PRINTF_H
 
-int	ft_printf(const char *format, ...);
-int	pf_int_to_str(t_u8_vec *vec, unsigned int n);
+#include "libft.h"
+
+/*
+** Flag constants for the parser
+*/
+
+# define LEADING_SPACE		0x01
+# define LEADING_PLUS		0x02
+# define PADDING_END		0x04
+# define LEADING_ZEROS		0x08
+# define ALTERNATIVE_FORM	0x10
+
+int				ft_printf(const char *format, ...);
+int				pf_int_to_str(t_u8_vec *vec, unsigned int n);
+unsigned char	parse_flags(const char *str);
 
 #endif
