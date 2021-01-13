fb1ff77fe4f633cb9e461a4ab968813bf69b8ec2
Oom Sveta <oomsveta@protonmail.com>
2021-01-13 14:02:34 +0100

    Handle multi-bytes char

--- src/formatter/pf_formatter_format_char.c
+++ src/formatter/pf_formatter_format_char.c
@@ -17,14 +17,27 @@
 ** char formatting is affected by '-' flag and width
 */
 
-// TODO: Handle size modifier (l)
 int	pf_format_char(t_format *format, t_u8_vec *buffer, va_list args)
 {
-	if (((format->flags & WIDTH_OR_PAD_END) == DEFINED_WIDTH
-		&& !pf_pad(buffer, format->width, ' '))
-		|| !u8_vec_push(buffer, va_arg(args, int))
-		|| ((format->flags & WIDTH_OR_PAD_END) == WIDTH_OR_PAD_END
-		&& !pf_pad(buffer, format->width, ' ')))
+	unsigned char	mbchar[4];
+	size_t			length;
+	size_t			i;
+
+	length = format->size == L ? utf32_to_utf8(mbchar, va_arg(args, int)) : 1;
+	i = 0;
+	if ((format->flags & WIDTH_OR_PAD_END) == DEFINED_WIDTH
+		&& !pf_pad(buffer, format->width - length, ' '))
+		return (0);
+	if (format->size == L)
+		while (i < length)
+		{
+			if (!u8_vec_push(buffer, mbchar[i++]))
+				return (0);
+		}
+	else if (!u8_vec_push(buffer, va_arg(args, int)))
+		return (0);
+	if ((format->flags & WIDTH_OR_PAD_END) == WIDTH_OR_PAD_END
+		&& !pf_pad(buffer, format->width - length, ' '))
 		return (0);
 	return (1);
 }
--- src/formatter/pf_formatter_format_str.c
+++ src/formatter/pf_formatter_format_str.c
@@ -30,13 +30,13 @@ int	pf_format_str(t_format *format, t_u8_vec *buffer, va_list args)
 	if (format->flags & DEFINED_PRECISION)
 		length = min(format->precision, length);
 	if ((format->flags & WIDTH_OR_PAD_END) == DEFINED_WIDTH
-		&& !pf_pad(buffer, format->width - length + 1, ' '))
+		&& !pf_pad(buffer, format->width - length, ' '))
 		return (0);
 	while (i < length)
 		if (!u8_vec_push(buffer, str[i++]))
 			return (0);
 	if ((format->flags & WIDTH_OR_PAD_END) == WIDTH_OR_PAD_END
-		&& !pf_pad(buffer, format->width - length + 1, ' '))
+		&& !pf_pad(buffer, format->width - length, ' '))
 		return (0);
 	return (1);
 }
